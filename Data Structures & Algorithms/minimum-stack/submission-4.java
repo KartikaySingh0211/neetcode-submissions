36ec5a18a4e0f4dc3a3ef07b6e110dd396c1114d
Kartikay Singh <kartikay.singh.0211@gmail.com>
2026-08-25 12:34:18 +0530

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.java
@@ -0,0 +1,29 @@
+class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty() || val <= minStack.peek()) minStack.push(val);
+    }
+    
+    public void pop() {
+        if(stack.isEmpty()) return;
+        int k = stack.pop();
+        if(k == minStack.peek()) minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
